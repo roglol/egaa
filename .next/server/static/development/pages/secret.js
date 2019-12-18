387573807f363f393390b08464d1cb3bc9fb064e
roglol <otikopotskhoraia@gmail.com>
2019-12-18 09:45:05 +0400

    auth

---
+++ .next/server/static/development/pages/secret.js
@@ -0,0 +1,334 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = require('../../../ssr-module-cache.js');
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/@babel/runtime-corejs2/core-js/object/define-property.js":
+/*!*******************************************************************************!*\
+  !*** ./node_modules/@babel/runtime-corejs2/core-js/object/define-property.js ***!
+  \*******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! core-js/library/fn/object/define-property */ "core-js/library/fn/object/define-property");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime-corejs2/helpers/esm/defineProperty.js":
+/*!***************************************************************************!*\
+  !*** ./node_modules/@babel/runtime-corejs2/helpers/esm/defineProperty.js ***!
+  \***************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return _defineProperty; });
+/* harmony import */ var _core_js_object_define_property__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../core-js/object/define-property */ "./node_modules/@babel/runtime-corejs2/core-js/object/define-property.js");
+/* harmony import */ var _core_js_object_define_property__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_core_js_object_define_property__WEBPACK_IMPORTED_MODULE_0__);
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    _core_js_object_define_property__WEBPACK_IMPORTED_MODULE_0___default()(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+/***/ }),
+
+/***/ "./pages/secret.js":
+/*!*************************!*\
+  !*** ./pages/secret.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/defineProperty */ "./node_modules/@babel/runtime-corejs2/helpers/esm/defineProperty.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! axios */ "axios");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react_cookie__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react-cookie */ "react-cookie");
+/* harmony import */ var react_cookie__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(react_cookie__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _utils_auth__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../utils/auth */ "./utils/auth.js");
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement;
+
+
+
+
+const serverUrl = 'http://localhost:3001'; // set up cookies
+
+const cookies = new react_cookie__WEBPACK_IMPORTED_MODULE_3__["Cookies"]();
+
+class Secret extends react__WEBPACK_IMPORTED_MODULE_1___default.a.Component {
+  constructor(...args) {
+    super(...args);
+
+    Object(_babel_runtime_corejs2_helpers_esm_defineProperty__WEBPACK_IMPORTED_MODULE_0__["default"])(this, "onPingCall", async e => {
+      const token = cookies.get('token');
+
+      try {
+        const res = await axios__WEBPACK_IMPORTED_MODULE_2___default.a.get(serverUrl + '/api/ping', {
+          headers: {
+            'Authorization': token
+          }
+        });
+        console.log(res.data.msg);
+      } catch (err) {
+        console.log(err.response.data.msg);
+      }
+    });
+  }
+
+  render() {
+    return __jsx("div", null, __jsx("h2", null, "Secret page"), __jsx("p", null, "Only accessible via a valid JWT"), __jsx("br", null), __jsx("button", {
+      onClick: e => this.onPingCall(e)
+    }, "Ping Call"), __jsx("p", null, "Check console for response"));
+  }
+
+} // Server-Side Rendering
+
+
+Secret.getInitialProps = async ctx => {
+  // Must validate JWT
+  // If the JWT is invalid it must redirect
+  // back to the main page. You can do that
+  // with Router from 'next/router
+  await Object(_utils_auth__WEBPACK_IMPORTED_MODULE_4__["handleAuthSSR"])(ctx); // Must return an object
+
+  return {};
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Secret);
+
+/***/ }),
+
+/***/ "./utils/auth.js":
+/*!***********************!*\
+  !*** ./utils/auth.js ***!
+  \***********************/
+/*! exports provided: handleAuthSSR */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "handleAuthSSR", function() { return handleAuthSSR; });
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "axios");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/router */ "next/router");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var react_cookie__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-cookie */ "react-cookie");
+/* harmony import */ var react_cookie__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(react_cookie__WEBPACK_IMPORTED_MODULE_2__);
+
+
+ // set up cookies
+
+const cookies = new react_cookie__WEBPACK_IMPORTED_MODULE_2__["Cookies"]();
+const serverUrl = 'http://localhost:3001';
+async function handleAuthSSR(ctx) {
+  let token = null; // if context has request info aka Server Side
+
+  if (ctx.req) {
+    // ugly way to get cookie value from a string of values
+    // good enough for demostration
+    token = ctx.req.headers.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  } else {
+    // we dont have request info aka Client Side
+    token = cookies.get('token');
+  }
+
+  try {
+    const response = await axios__WEBPACK_IMPORTED_MODULE_0___default.a.get(serverUrl + "/api/token/ping", {
+      headers: {
+        'Authorization': token
+      }
+    });
+    console.log("token ping:", response.data.msg);
+  } catch (err) {
+    // in case of error
+    console.log(err.response.data.msg);
+    console.log("redirecting back to main page"); // redirect to login
+
+    if (ctx.res) {
+      ctx.res.writeHead(302, {
+        Location: '/'
+      });
+      ctx.res.end();
+    } else {
+      next_router__WEBPACK_IMPORTED_MODULE_1___default.a.push('/');
+    }
+  }
+}
+
+/***/ }),
+
+/***/ 5:
+/*!*******************************!*\
+  !*** multi ./pages/secret.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\Users\Roglol\Desktop\dekori\pages\secret.js */"./pages/secret.js");
+
+
+/***/ }),
+
+/***/ "axios":
+/*!************************!*\
+  !*** external "axios" ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("axios");
+
+/***/ }),
+
+/***/ "core-js/library/fn/object/define-property":
+/*!************************************************************!*\
+  !*** external "core-js/library/fn/object/define-property" ***!
+  \************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("core-js/library/fn/object/define-property");
+
+/***/ }),
+
+/***/ "next/router":
+/*!******************************!*\
+  !*** external "next/router" ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("next/router");
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "react-cookie":
+/*!*******************************!*\
+  !*** external "react-cookie" ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("react-cookie");
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=secret.js.map
---
+++ server.js
@@ -0,0 +1,83 @@
+const next = require('next')
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const jwt = require('jsonwebtoken');
+const jwtSecret = "mysuperdupersecret";
+
+const uri = `mongodb://granot:granot1717@ds157185.mlab.com:57185/granot`;
+
+app.prepare()
+ .then(() => {
+     const Api = express.Router()
+     
+     const server = express() 
+     server.use('/api', Api)
+     Api.use(bodyParser.json()) 
+     Api.use(bodyParser.urlencoded({extended:true}))
+     Api.use(function (req, res, next) {
+      // Allow Origins
+      res.header("Access-Control-Allow-Origin", "*");
+      // Allow Methods
+      res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+      // Allow Headers
+      res.header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Authorization");
+      // Handle preflight, it must return 200
+      if (req.method === "OPTIONS") {
+        // Stop the middleware chain
+        return res.status(200).end();
+      }
+      // Next middleware 
+      next();
+    });
+
+    Api.use((req, res, next) => {   
+      if (req.path == '/login') {
+        return next()
+      }
+      const token = req.headers.authorization
+      try {
+        var decoded = jwt.verify(token, jwtSecret);
+        console.log("decoded", decoded)
+      } catch (err) {
+        return res.status(401).json({ "msg": err.message })
+      }
+      next()
+    });
+
+    server.get('/favicon.ico', (req, res) => res.status(204))
+
+    Api.get("/ping", (req, res) => {
+      // random endpoint so that the client can call something
+      res.json({ "msg": "pong" })
+    });
+
+    Api.get("/token/ping", (req, res) => {
+      // random endpoint so that the client can call something
+      res.json({ "msg": "pong" })
+    });
+
+
+    Api.get("/login", (req, res) => {
+      // generate a constant token, no need to be fancy here
+      const token = jwt.sign({ "username": "Mike" }, jwtSecret, { expiresIn: 6000000000 }) // 1 min token
+      // return it back
+      res.json({ "token": token })
+    });
+
+     server.get('*', (req,res) =>{
+         return handle(req,res)
+     })
+
+  server.listen(3001, err => {
+    if (err) {
+      throw err
+    }
+
+    console.log(`> Ready on http://localhost:${3001}`)
+  })
+})
